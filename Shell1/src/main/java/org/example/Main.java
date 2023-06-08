d473f2901b2b4116da0b29626fcb427118214201
EremenkoSA <135714875+EremenkoSA@users.noreply.github.com>
2023-06-08 11:30:26 +0300

    2
    2

---
+++ Shell1/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+
+public class Main {
+    public static HashMap<String, Command> ComList = new HashMap<>();
+
+
+    public static void main(String[] args) {
+
+        Command quit = new Quit();
+        ComList.put(quit.getName(), quit);
+
+        Command time = new Time();
+        ComList.put(time.getName(), time);
+
+        Command date = new Date();
+        ComList.put(date.getName(), date);
+
+
+        while (true) {
+            System.out.print(">");
+            Scanner s = new Scanner(System.in);
+            String string = s.nextLine();
+            String[] words = string.split("\\s+");
+            if (ComList.containsKey(words[0])) {
+                ComList.get(words[0]).exec(words);
+            } else {
+                System.out.println("не является внутренней или внешней командой");
+            }
+        }
+    }
+}
